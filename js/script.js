537672207ba316dccc3d4f1a87e1464f8cd42941
Alikhalid2020 <lanrakhaled@gmail.com>
2020-10-03 09:28:54 +0300

    added script.js, added bootstrap.css

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var submission = function() {
+    const daysOfTheWeek =[
+        "Sundays",
+        "Monday",
+        "Tuesday",
+        "Wensday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const maleName =[
+        "Kwasi",
+        "kwadwo",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame",
+    ];
+    const femaleName =[
+        "Akasou",
+        "Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama",
+    ];
+}

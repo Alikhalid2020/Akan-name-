c18b80923f4289567ef87470e988fb2d443aae11
Alikhalid2020 <lanrakhaled@gmail.com>
2020-10-04 23:26:59 +0300

    added , to js in the female names

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ var submission = function() {
      "Akua",
      "Yaa",
      "Afua",
-     "Ama"
+     "Ama",
     ];
     let month = parseInt(document.getElementById("month").value);
     let year = parseInt(document.getElementById("year").value);
